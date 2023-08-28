40ce229a7819cba2f32ef9df3f5fce06d86a22c2
nazmulhossain17 <nazmulhossain17sn@gmail.com>
2023-08-28 12:08:06 +0600

    controller

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const helmet = require("helmet");
 const path = require("path");
+const multer = require("multer");
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
 app.use(express.json());
 app.use(helmet());
@@ -14,6 +17,19 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/assets");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage });
+
+/* ROUTES WITH FILES */
+app.post("/auth/register", upload.single("picture"), register);
 
 app.get("/", (req, res) => {
   res.send("Working");
