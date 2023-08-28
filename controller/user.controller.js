704d354f3ebd97abf8ab9f7b046b61ad91cd74a2
nazmulhossain17 <nazmulhossain17sn@gmail.com>
2023-08-28 20:00:51 +0600

    user.controller.js

---
+++ controller/user.controller.js
@@ -0,0 +1,31 @@
+const User = require("../model/User");
+
+async function getUser(req, res) {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+}
+
+async function getUserFriends(req, res) {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id);
+    const friends = await Promise.all(
+      user.friends.map((id) => User.findById(id))
+    );
+    const formattedFriends = friends.map(
+      ({ _id, firstName, lastName, occuption, location, picturePath }) => {
+        return { _id, firstName, lastName, occuption, location, picturePath };
+      }
+    );
+    res.status(200).json(formattedFriends);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+}
+
+module.exports = { getUser, getUserFriends };
--- routes/user-router.js
+++ routes/user-router.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const { register, login } = require("../controller/auth.controller");
 const upload = require("../upload/upload");
+const verifyToken = require("../middleware/auth");
+const { getUser, getUserFriends } = require("../controller/user.controller");
 const router = express.Router();
 
 router.get("/hello", (req, res) => {
@@ -9,5 +11,8 @@ router.get("/hello", (req, res) => {
 
 router.post("/auth/register", upload.single("picture"), register);
 router.post("/auth/login", login);
+router.get("/:id", verifyToken, getUser);
+router.get("/:id/friends", verifyToken, getUserFriends);
+router.patch("/:id/:friendId", verifyToken);
 
 module.exports = router;
