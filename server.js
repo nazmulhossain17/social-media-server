572a25dfaf11fde74d5f121721261d9308bd2bf3
nazmulhossain17 <nazmulhossain17sn@gmail.com>
2023-08-28 21:48:06 +0600

    insert fake data

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const app = require("./app");
 const { port, dbURL } = require("./config");
 const mongoose = require("mongoose");
+const User = require("./model/User");
+const Post = require("./model/Post");
+// const { users, posts } = require("./data");
 
 const connectDB = async () => {
   try {
+    // mongoose.connect(dbURL).then(() => {
+    //   User.insertMany(users);
+    //   Post.insertMany(posts);
+    // });
     await mongoose.connect(dbURL);
     console.log("Database Connected!");
     app.listen(port, () => {
