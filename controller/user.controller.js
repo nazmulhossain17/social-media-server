bebfdbc7a6e19356d3d51029a38f6361f8468f70
nazmulhossain17 <nazmulhossain17sn@gmail.com>
2023-08-28 20:26:22 +0600

    user.controller.js

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -28,4 +28,35 @@ async function getUserFriends(req, res) {
   }
 }
 
-module.exports = { getUser, getUserFriends };
+async function removeAddFriend(req, res) {
+  try {
+    const { id, friendId } = req.params;
+    const user = await User.findById(id);
+    const friend = await User.findById(friendId);
+
+    if (user.friends.includes(friendId)) {
+      user.friends = user.friends.filter((id) => id !== friendId);
+      friend.friends = friend.friends.filter((id) => id !== id);
+    } else {
+      user.friends.push(friendId);
+      friend.friends.push(id);
+    }
+    await user.save();
+    await friend.save();
+
+    const friends = await Promise.all(
+      user.friends.map((id) => User.findById(id))
+    );
+    const formattedFriends = friends.map(
+      ({ _id, firstName, lastName, occupation, location, picturePath }) => {
+        return { _id, firstName, lastName, occupation, location, picturePath };
+      }
+    );
+
+    res.status(200).json(formattedFriends);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+}
+
+module.exports = { getUser, getUserFriends, removeAddFriend };
--- routes/user-router.js
+++ routes/user-router.js
@@ -2,7 +2,11 @@ const express = require("express");
 const { register, login } = require("../controller/auth.controller");
 const upload = require("../upload/upload");
 const verifyToken = require("../middleware/auth");
-const { getUser, getUserFriends } = require("../controller/user.controller");
+const {
+  getUser,
+  getUserFriends,
+  removeAddFriend,
+} = require("../controller/user.controller");
 const router = express.Router();
 
 router.get("/hello", (req, res) => {
@@ -13,6 +17,6 @@ router.post("/auth/register", upload.single("picture"), register);
 router.post("/auth/login", login);
 router.get("/:id", verifyToken, getUser);
 router.get("/:id/friends", verifyToken, getUserFriends);
-router.patch("/:id/:friendId", verifyToken);
+router.patch("/:id/:friendId", verifyToken, removeAddFriend);
 
 module.exports = router;
